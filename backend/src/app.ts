13bb7ec85120469145e4604fb14b4f0ac3060b73
Sugata <thcdesigning@gmail.com>
2024-11-29 17:15:15 +0530

    refactor: More readable names for backend actions.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,9 +3,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-import logs from "./actions/logs";
-import authUrl from "./actions/auth/google/url";
-import authCallback from "./actions/auth/google/callback";
+import logsAction from "./actions/logs.post";
+import authUrlAction from "./actions/auth/google/url";
+import authCallbackAction from "./actions/auth/google/callback";
 
 const app: Application = express();
 const port: number = 3001;
@@ -29,9 +29,9 @@ app.post("/log", (req, res) => {
   console.log(req.body);
   res.send("Logged");
 });
-app.get("/logs", logs);
-app.get("/auth/google/url", authUrl);
-app.get("/auth/google/callback", authCallback);
+app.get("/logs", logsAction);
+app.get("/auth/google/url", authUrlAction);
+app.get("/auth/google/callback", authCallbackAction);
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
